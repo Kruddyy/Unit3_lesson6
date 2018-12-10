6b0b4f3a32d491711ec3ec5d22194c98a6ac92be
Kruddyy <43756292+Kruddyy@users.noreply.github.com>
2018-12-10 12:30:41 -0500

    Add files via upload

---
+++ lesson 6/problem 3/problem1.py
@@ -0,0 +1,6 @@
+name = input('What is your name?')
+print('UGH, I HATE YOU ' + name)
+color = input('What is your favorite color? ')
+print( 'UGh, I HATE ' + color + ' THERE ARE SO MANY BETTER COLORS')
+subject = input( 'What is your favorite subject in school? ')
+print('UGH, I HATE ' + subject + ' I HATE EVERYTHING')
